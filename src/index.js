1be02ac9b8dc7091d1bec2bad4c40488f0799786
Gregory Murray <gr.j.murray@gmail.com>
2024-11-06 04:09:46 -0800

    deliverable 2 complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 // index.js
 
+//import VirtualConsoleLogEntryStringifier from "happy-dom/lib/console/utilities/VirtualConsoleLogEntryStringifier.js";
+
+//////////////////////////////////////////////////////
+//selectors defined in functions per Lantz on discord
+/////////////////////////////////////////////////////
+
+
+
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  const detailImg = document.querySelector("#ramen-detail > .detail-image");
+  const detailName = document.querySelector("#ramen-detail > .name");
+  const detailRestaurant = document.querySelector("#ramen-detail > .restaurant");
+  const detailsRating = document.getElementById("rating-display");
+  const detailsComment = document.getElementById("comment-display");
+  
+  detailImg.src = ramen.image;
+  detailName.textContent = ramen.name;
+  detailRestaurant.textContent = ramen.restaurant;
+  detailsRating.textContent = ramen.rating;
+  detailsComment.textContent = ramen.comment;
+  console.log("hi");
 };
+  
+
+
+
 
 const addSubmitListener = () => {
   // Add code
@@ -11,7 +34,6 @@ const addSubmitListener = () => {
 
 const displayRamens = () => {
   const ramenMenuDiv = document.querySelector('#ramen-menu')
-  console.log(ramenMenuDiv)
   return fetch("http://localhost:3000/ramens")
   .then(resp=>resp.json())
   .then(ramens =>{
@@ -19,10 +41,13 @@ const displayRamens = () => {
       let img = document.createElement("img")
       img.src = `${ramen.image}`
       ramenMenuDiv.appendChild(img)
-      // img.addEventListener("click", event=>{}
+      console.log(`${ramen}`)
+      //we all need closure sometimes
+      img.addEventListener("click", () =>handleClick(ramen))})})}
+      
+      
          
-})})
-};
+
 
 const main = () => {
   displayRamens()
@@ -32,6 +57,8 @@ const main = () => {
 main()
 
 // Export functions for testing
+
+
 export {
   displayRamens,
   addSubmitListener,
